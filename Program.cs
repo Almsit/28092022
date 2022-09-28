3c2aaaa2102d5f02c6f8ea36fa205fe442629502
«Almsit» <ZulAman@yandex.ru>
2022-09-28 16:04:36 +0300

    2. Добавить проверку на пустой ввод массива

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿Console.Clear();
 Console.WriteLine("Введите числа через пробел для создания массива");
 string[] stringArr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+
+bool Valid(string[] stringArr){
+    if(stringArr.Length == 0){
+        Console.WriteLine("Массив задан пустым");
+        return false;
+    }
+    return true;
+}
+bool valid =  Valid(stringArr);
+
+if(!valid){
+    return;
+}
